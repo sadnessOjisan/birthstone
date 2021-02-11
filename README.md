30987a97ff1fdbf4571bd68c0eb7fd97ee235820
sadnessOjisan <sadness.ojisan@gmail.com>
2021-02-12 04:35:13 +0900

    show today (#24)
    * cmt out logger
    
    * can get hello
    
    * show today
    
    * date
    
    * fix readme

--- README.md
+++ README.md
@@ -2,25 +2,8 @@
 
 ソシャゲの周年記念一覧。この日付近に始めるとたくさん石を貰えるよ
 
-## ゲーム情報提供・修正について
+## ゲームの情報提供・修正について
 
-お気軽に PR をいただければ 🙇‍♀️🙇‍♀️🙇‍♀️
+お気軽に PR/Issue をいただけると幸いです 🙇‍♀️🙇‍♀️🙇‍♀️
 
-### 情報提供
-
-src/data.yaml へ追加をお願いします 🙇‍♀️
-
-### Dev
-
-```
-$ cargo install
-
-$ wasm-pack build --target web --out-name wasm --out-dir ./static
-
-# static を serve する何かしらのコマンド
-$ miniserve ./static --index index.html
-```
-
-### 詳しくは
-
-詳しくは[CONTRIBUTING.md](./.github/CONTRIBUTING.md) をご覧ください。
+詳しくは [CONTRIBUTING.md](./.github/CONTRIBUTING.md) をご覧ください。
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,14 @@ extern crate serde_yaml;
 use calendarize;
 use chrono::*;
 use std::collections::HashMap;
+use std::time::SystemTime;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use yew::web_sys;
+#[wasm_bindgen]
+extern "C" {
+    fn getISOString() -> String;
+}
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 struct Game {
@@ -26,6 +32,7 @@ struct Model {
     selected_month: u32,
     raw_games: Vec<Game>,
     games: MonthData,
+    today: NaiveDate,
 }
 
 const CURRENT_YEAR: i32 = 2021; // TODO: 日付ライブラリから取得する
@@ -72,8 +79,13 @@ impl Model {
     fn render_day(&self, day_games: &MonthDateGames) -> Html {
         match day_games {
             Some(day_games) => {
+                let is_selected_month = self.today.month() == day_games.0 .0;
+                let is_selected_day = self.today.day() == day_games.0 .1;
+                let is_selected = is_selected_day && is_selected_month;
+      
+
                 html! {
-                    <td>
+                    <td class={if is_selected {"selected"} else {""}}>
                     <div class="day">{day_games.0.1}</div>
                     <div class="games">
                     {for day_games.1.iter().map(|game| {html! {<a href={game.url.to_string()}>{game.title.to_string()}</a>}})}
@@ -93,6 +105,11 @@ enum Msg {
     PrevMonth,
 }
 
+#[no_mangle]
+pub fn get_iso() -> String {
+    getISOString()
+}
+
 fn create_games_date(games: &Vec<Game>, month: u32) -> MonthData {
     let date = NaiveDate::from_ymd(CURRENT_YEAR, month, 1);
     let calendar = calendarize::calendarize(date);
@@ -107,7 +124,7 @@ fn create_games_date(games: &Vec<Game>, month: u32) -> MonthData {
                 .collect();
             match day {
                 0 => {
-                    log::error!("unexpected date: {:?}", day);
+                    // log::error!("unexpected date: {:?}", day);
                     m.entry((month, *day)).or_insert(None);
                 }
                 _ => {
@@ -159,6 +176,12 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let iso = get_iso(); // 宙に浮いた参照を防ぐ
+        log::info!("interop check {:?}",  iso);
+        let current_date = iso.as_str();
+        let parse_from_str = NaiveDate::parse_from_str;
+        let today =
+            parse_from_str(current_date, "%Y-%m-%d").unwrap_or(NaiveDate::from_ymd(2021, 1, 1));
         let s = include_str!("./data.yaml");
         let games: Vec<Game> = serde_yaml::from_str(s).unwrap();
         let game_and_date = create_games_date(&games, CURRENT_MONTH);
@@ -167,6 +190,7 @@ impl Component for Model {
             games: game_and_date,
             selected_month: CURRENT_MONTH,
             raw_games: games,
+            today: today,
         }
     }
 
