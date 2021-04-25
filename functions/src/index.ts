29f40b645632e7f2e4c25175d4ccd5cc8ab5bd81
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-25 14:58:01 +0900

    use 12

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from "firebase-functions";
+import yaml from "./data.yaml";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+export const helloWorld2 = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
