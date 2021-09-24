83164f1948d1fbb9306480c208cc7553b347d81b
Yuta Ide <sadness.ojisan@gmail.com>
2021-09-25 03:44:16 +0900

    ソシャゲデータを返すエンドポイントを作成 (#46)
    * return data
    
    * add scafold

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import { data } from "./data";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,8 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+export const getData = functions.https.onRequest((request, response) => {
+  response.json(data);
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default () => {
   return <div>hello world!!</div>;
 };
+
+export const getStaticProps = () => {
+  return {};
+};
