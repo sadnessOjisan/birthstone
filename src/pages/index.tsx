6d418575cd78085fc4dfc0276ef2a39aadca648c
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-25 03:59:43 +0900

    update ssg props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-export default () => {
-  return <div>hello world!!</div>;
+import { GetStaticProps } from "next";
+import { VFC } from "react";
+import { ResponseType, schema } from "../schema";
+
+type Props = {
+  data: ResponseType;
 };
 
-export const getStaticProps = () => {
-  return {};
+const Root: VFC<Props> = (props) => {
+  return <div>hello {JSON.stringify(props.data)}!!</div>;
+};
+
+export default Root;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await fetch(
+    "https://us-central1-birthstone-b73d7.cloudfunctions.net/getData"
+  );
+
+  const parsed = schema.parse(data);
+
+  return {
+    props: {
+      data: parsed,
+    },
+  };
 };
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,1 +1,11 @@
-export const schema = {};
+import { z } from "zod";
+
+const item = z.object({
+  title: z.string(),
+  published: z.string(),
+  url: z.string(),
+});
+
+export const schema = z.array(item);
+
+export type ResponseType = z.infer<typeof schema>;
