f6d5ca47d70b1bd3807e65af94d6bdc256d4b7e0
Yuta Ide <sadness.ojisan@gmail.com>
2021-09-25 12:29:49 +0900

    URL から SSG (#47)
    * update ssg props
    
    * fix to get data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-export default () => {
-  return <div>hello world!!</div>;
+import { GetStaticProps } from "next";
+import { VFC } from "react";
+import { ResponseType, schema } from "../schema";
+
+type Props = {
+  data: ResponseType;
 };
 
-export const getStaticProps = () => {
-  return {};
+const Root: VFC<Props> = (props) => {
+  return <div>hello {JSON.stringify(props.data)}!!</div>;
+};
+
+export default Root;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await fetch(
+    "https://us-central1-birthstone-b73d7.cloudfunctions.net/getData"
+  );
+
+  const data = await res.json();
+
+  try {
+    const parsed = schema.parse(data);
+
+    return {
+      props: {
+        data: parsed,
+      },
+    };
+  } catch (e) {
+    console.error(data);
+    console.error(e);
+  }
 };
