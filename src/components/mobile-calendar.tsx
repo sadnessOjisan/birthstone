f8259140d31712707dff17ff50fded10550bad20
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-26 15:21:38 +0900

    cell styling

--- src/components/mobile-calendar.tsx
+++ src/components/mobile-calendar.tsx
@@ -1,6 +1,7 @@
 import { VFC } from "react";
 import { ResponseType } from "../schema";
 import { Calendar as CalendarType } from "../type";
+import { css } from "../util/stitches.config";
 
 type Props = {
   calendar: CalendarType;
@@ -8,20 +9,59 @@ type Props = {
   data: ResponseType;
 };
 
+const styles = {
+  item: css({
+    display: "flex",
+    alignItems: "center",
+    padding: 12,
+    fontSize: "1.8rem",
+    border: "solid 1px #ffffff",
+    borderBottom: "none",
+    "&:last-child": {
+      border: "solid 1px #ffffff",
+    },
+  }),
+  gameListItem: css({
+    listStyle: "none",
+    marginBottom: 12,
+    "&:last-child": {
+      marginBottom: 0,
+    },
+  }),
+  dateBlock: css({
+    marginRight: 24,
+  }),
+  link: css({
+    color: "#a9abbc",
+    textDecoration: "none",
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  }),
+};
+
 export const MobileCalendar: VFC<Props> = (props) => {
   return (
     <div>
-      {props.calendar.flat().map((date) => (
-        <div>
-          <div>
-            {date.game.map((g) => (
-              <li>
-                <a href={g.url}>{g.title}</a>
-              </li>
-            ))}
+      {props.calendar
+        .flat()
+        .filter((d) => d.date !== undefined)
+        .map((date) => (
+          <div className={styles.item()}>
+            <div className={styles.dateBlock()}>
+              {date.date.getMonth() + 1}/{date.date.getDate()}
+            </div>
+            <ul>
+              {date.game.map((g) => (
+                <li className={styles.gameListItem()}>
+                  <a href={g.url} className={styles.link()}>
+                    {g.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
