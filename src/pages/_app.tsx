5de95fa5c59f0ed7640f81b6b745a5b58ae532a3
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-03 18:33:49 +0900

    update component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,39 @@
 import { getMessaging, onMessage } from "firebase/messaging";
 import { useEffect } from "react";
-import { firebaseCloudMessaging } from "../util/web-push";
+import type { AppProps /*, AppContext */ } from "next/app";
 
-useEffect(() => {
-  // setToken();
-  // async function setToken() {
-  //   try {
-  //     const token = await firebaseCloudMessaging.init();
-  //     if (token) {
-  //       getMessage();
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-  // function getMessage() {
-  //   const messaging = getMessaging();
-  //   console.log({ messaging });
-  //   messaging.onMessage((message) => {
-  //     const { title, body } = JSON.parse(message.data.notification);
-  //     var options = {
-  //       body,
-  //     };
-  //     self.registration.showNotification(title, options);
-  //   });
-  // }
-  const messaging = getMessaging();
-  onMessage(messaging, (payload) => {
-    console.log("Message received. ", payload);
-    // ...
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // setToken();
+    // async function setToken() {
+    //   try {
+    //     const token = await firebaseCloudMessaging.init();
+    //     if (token) {
+    //       getMessage();
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // function getMessage() {
+    //   const messaging = getMessaging();
+    //   console.log({ messaging });
+    //   messaging.onMessage((message) => {
+    //     const { title, body } = JSON.parse(message.data.notification);
+    //     var options = {
+    //       body,
+    //     };
+    //     self.registration.showNotification(title, options);
+    //   });
+    // }
+    const messaging = getMessaging();
+    onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload);
+      // ...
+    });
   });
-});
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
