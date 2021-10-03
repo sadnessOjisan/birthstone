43f7d1ab8f78c04712f5d8656b0ecd1c444a9ff8
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-03 23:03:45 +0900

    add fb

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,42 @@
-importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
+// importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyARcJXJRq2heclH1DoMu3zqkzwb0vuA4iw",
-  authDomain: "birthstone-b73d7.firebaseapp.com",
-  projectId: "birthstone-b73d7",
-  storageBucket: "birthstone-b73d7.appspot.com",
-  messagingSenderId: "55130399678",
-  appId: "1:55130399678:web:3c9a4711499f16fdfd0fef",
-  measurementId: "G-FQH4QWQ628",
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyARcJXJRq2heclH1DoMu3zqkzwb0vuA4iw",
+//   authDomain: "birthstone-b73d7.firebaseapp.com",
+//   projectId: "birthstone-b73d7",
+//   storageBucket: "birthstone-b73d7.appspot.com",
+//   messagingSenderId: "55130399678",
+//   appId: "1:55130399678:web:3c9a4711499f16fdfd0fef",
+//   measurementId: "G-FQH4QWQ628",
+// });
 
-firebase.messaging();
+// firebase.messaging();
 
-//background notifications will be received here
-firebase.messaging().setBackgroundMessageHandler((payload) => {
-  const { title, body } = JSON.parse(payload.data.notification);
-  var options = {
-    body,
+// //background notifications will be received here
+// firebase.messaging().setBackgroundMessageHandler((payload) => {
+//   const { title, body } = JSON.parse(payload.data.notification);
+//   var options = {
+//     body,
+//     icon: "/icon-192x192.png",
+//   };
+//   registration.showNotification(title, options);
+// });
+import { getMessaging } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw";
+
+const messaging = getMessaging();
+onBackgroundMessage(messaging, (payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
     icon: "/icon-192x192.png",
   };
-  registration.showNotification(title, options);
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
