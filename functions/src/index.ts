28d454b87a3330d817d4c7e02f362cb7b69d1ba4
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-09 23:38:15 +0900

    add batch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 import { data } from "./data";
 
 // // Start writing Firebase Functions
@@ -12,3 +13,49 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 export const getData = functions.https.onRequest((request, response) => {
   response.json(data);
 });
+
+export const sendNotificationOfTodayGame = functions.pubsub
+  .schedule("every 24 hours")
+  .onRun((context) => {
+    const today = new Date();
+    const todayIsBirthdayGames = data.filter((item) => {
+      const publishDate = new Date(item.published);
+      if (
+        today.getMonth() === publishDate.getMonth() &&
+        today.getDate() === publishDate.getDate()
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (todayIsBirthdayGames.length === 0) {
+      return null;
+    }
+    admin
+      .firestore()
+      .collection("token")
+      .get()
+      .then((snapshot) => {
+        const tokens = snapshot.docs.map((d) => {
+          return d.data().token as string;
+        });
+        const message = {
+          data: {
+            title: todayIsBirthdayGames[0].title,
+          },
+          tokens: tokens,
+        };
+
+        admin
+          .messaging()
+          .sendMulticast(message)
+          .then((response) => {
+            console.log(
+              response.successCount + " messages were sent successfully"
+            );
+          });
+      });
+
+    return null;
+  });
